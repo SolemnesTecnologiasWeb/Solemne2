7a9645a0dd859037a269b304ab445ae42ec2901a
Valentina Hernandez <valeliberona123@gmail.com>
2025-06-07 21:01:50 -0400

    Búsqueda de citas por paciente o médico.

--- src/app/modules/cita/pages/historial-citas/historial-citas.component.ts
+++ src/app/modules/cita/pages/historial-citas/historial-citas.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableDataSource } from '@angular/material/table';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-historial-citas',
@@ -9,12 +13,14 @@ import { MatChipsModule } from '@angular/material/chips';
   imports: [
     CommonModule,
     MatTableModule,
-    MatChipsModule
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './historial-citas.component.html',
   styleUrls: ['./historial-citas.component.css']
 })
-export class HistorialCitasComponent {
+export class HistorialCitasComponent implements OnInit{
   displayedColumns: string[] = ['paciente', 'medico', 'fecha', 'hora', 'estado'];
 
   citas = [
@@ -41,6 +47,8 @@ export class HistorialCitasComponent {
     }
   ];
 
+  dataSource = new MatTableDataSource(this.citas);
+
   getColor(estado: string): string {
     switch (estado) {
       case 'Confirmada': return 'primary';
@@ -48,5 +56,14 @@ export class HistorialCitasComponent {
       case 'Cancelada': return 'warn';
       default: return '';
     }
-  }  
+  }
+  
+  ngOnInit() {
+    this.dataSource.filterPredicate = (data, filter) => {
+      const paciente = data.paciente.toLowerCase();
+      const medico = data.medico.toLowerCase();
+      return paciente.includes(filter) || medico.includes(filter);
+    };
+  }
+  
 }
